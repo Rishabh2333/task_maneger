cb219ca3952ee1754bc208318d52342d6689ee32
Rishabh2333 <annaturecreation2323@gmail.com>
2026-02-01 13:11:44 +0530

    Convert backend to ES modules for Render

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-const app = require("./src/app");
+import app from "./src/app.js";
 
 const connectDB = require("./src/config/db");
 import cors from "cors";
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
-const express = require("express");
-const cors = require("cors");
+
+import express from "express";
+import cors from "cors";
 const helmet = require("helmet");
 const morgan = require("morgan");
 require("dotenv").config();
@@ -21,4 +22,4 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-module.exports = app;
+export default app;
