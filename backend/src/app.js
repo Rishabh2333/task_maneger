858a57a3852c8ebaec6897ad08086f906c436f43
Rishabh2333 <annaturecreation2323@gmail.com>
2026-02-05 23:40:09 +0530

    cors setup completed

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,15 +10,7 @@ const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://task-maneger.vercel.app"
-    ],
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
